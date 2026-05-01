61f94eb731ebd258d58de770bfed4807a4e9b72d
TommasoLapenna <tommaso.lapenna@edu.unifi.it>
2026-05-02 00:41:01 +0200

    Hidden navbar on scroll

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
+console.log("Hello World!");
+
 let prevScrollPosition = window.pageYOffset;
 window.onscroll = function() {
     let currentScrollPos = window.pageYOffset;
     if (prevScrollPosition > currentScrollPos) {
         document.getElementById("nav-container").style.top = "0";
     } else {
-        document.getElementById("navbar-container").style.top = "-50px";
+        document.getElementById("nav-container").style.top = "-50px";
     }
     prevScrollPosition = currentScrollPos;
 }
